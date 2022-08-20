34bd19ff12de0c5f3f8e5f72e8c8b811e3ec3d83
Borhan Uddin <68281712+wapborhan@users.noreply.github.com>
2022-08-20 18:19:49 +0600

    Upload

--- assets/js/header.js
+++ assets/js/header.js
@@ -1,9 +1,7 @@
 document.getElementById("header").innerHTML = `
 <nav class="navbar navbar-expand-lg bg-primary ">
       <div class="container">
-        <a class="navbar-brand logo" href="#">
-        <img src="assets/img/logo.png"/>
-        </a>
+        <img src="assets/img/logo.png" class="logo"/>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
           aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
@@ -11,16 +9,7 @@ document.getElementById("header").innerHTML = `
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
           <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
             <li class="nav-item">
-              <a class="nav-link active" aria-current="page" href="#">Home</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">Link</a>
-            </li>
-            <li class="nav-item dropdown">
-              <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
-                aria-expanded="false">
-                Dropdown
-              </a>
+              <a class="nav-link active" aria-current="page" href="https://code.wapborhan.com">Project</a>
             </li>
           </ul>
         </div>
