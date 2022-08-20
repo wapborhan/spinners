f366d9856ced56b397d67d9c64bf33ee78eb1d6d
Borhan Uddin <68281712+wapborhan@users.noreply.github.com>
2022-08-20 17:26:29 +0600

    UI Upload

---
+++ assets/js/footer.js
@@ -0,0 +1,6 @@
+document.getElementById("footer").innerHTML = `
+<div class="footer  p-3 text-center bg-dark text-light mt-4">
+      © 2022. All rights reserved | Design BY <a class="text-decoration-none text-warning"
+        href="https://srdreamlab.com">SR Dream Lab</a>.
+    </div>
+`;
