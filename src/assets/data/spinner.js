a253f408646549ad77443dae2f8c9819c6e67fc0
Borhan Uddin <68281712+wapborhan@users.noreply.github.com>
2022-09-08 14:10:35 +0600

    grg

--- src/assets/data/spinner.js
+++ src/assets/data/spinner.js
@@ -3,7 +3,66 @@ const DATA = [
     id: 1,
     name: "Spinner 1",
     class: "loader1",
-    code: ``,
+    code: `
+    .loader1,
+    .loader1:before,
+    .loader1:after {
+      background: red;
+      -webkit-animation: load1 1s infinite ease-in-out;
+      animation: load1 1s infinite ease-in-out;
+      width: 1em;
+      height: 4em;
+    }
+    .loader1 {
+      color: red;
+      text-indent: -9999em;
+      margin: 88px auto;
+      position: relative;
+      font-size: 11px;
+      -webkit-transform: translateZ(0);
+      -ms-transform: translateZ(0);
+      transform: translateZ(0);
+      -webkit-animation-delay: -0.16s;
+      animation-delay: -0.16s;
+    }
+    .loader1:before,
+    .loader1:after {
+      position: absolute;
+      top: 0;
+      content: "";
+    }
+    .loader1:before {
+      left: -1.5em;
+      -webkit-animation-delay: -0.32s;
+      animation-delay: -0.32s;
+    }
+    .loader1:after {
+      left: 1.5em;
+    }
+    @-webkit-keyframes load1 {
+      0%,
+      80%,
+      100% {
+        box-shadow: 0 0;
+        height: 4em;
+      }
+      40% {
+        box-shadow: 0 -2em;
+        height: 5em;
+      }
+    }
+    @keyframes load1 {
+      0%,
+      80%,
+      100% {
+        box-shadow: 0 0;
+        height: 4em;
+      }
+      40% {
+        box-shadow: 0 -2em;
+        height: 5em;
+      }
+    }`,
   },
   {
     id: 2,
--- src/components/body/Spinner.jsx
+++ src/components/body/Spinner.jsx
@@ -1,108 +1,37 @@
-import React from "react";
+import React, { Component } from "react";
+import DATA from "../../assets/data/spinner";
 
-const Spinner = () => {
-  return (
-    <div className="pbd-5j">
-      <div className="container">
-        <div className="row">
-          <div className="col-md-3">
-            <div className="card">
-              <div className="card-body h-250">
-                <div className="loader1" id="loader1">
-                  Loading...
+export default class Spinner extends Component {
+  state = {
+    spinnerdata: DATA,
+  };
+  render() {
+    console.log(this.state.spinnerdata);
+    return (
+      <div className="pbd-5j">
+        <div className="container">
+          <div className="row">
+            {this.state.spinnerdata.map((item) => {
+              return (
+                <div className="col-md-3" key={item.id}>
+                  <div className="card">
+                    <div className="card-header">{item.name}</div>
+                    <div className="card-body h-250">
+                      <div className={item.class} id="loader1"></div>
+                      {/* <div className="card">{item.code}</div> */}
+                    </div>
+                    <div className="card-footer">
+                      <button className="btn btn-success w-100">
+                        Source Code
+                      </button>
+                    </div>
+                  </div>
                 </div>
-              </div>
-              <div className="card-footer">
-                <button className="btn btn-success w-100">Source Code</button>
-              </div>
-            </div>
-          </div>
-          <div className="col-md-3">
-            <div className="card">
-              <div className="card-body h-250">
-                <div className="loader2" id="loader2">
-                  Loading...
-                </div>
-              </div>
-              <div className="card-footer">
-                <button className="btn btn-success w-100">Source Code</button>
-              </div>
-            </div>
-          </div>
-          <div className="col-md-3">
-            <div className="card">
-              <div className="card-body h-250">
-                <div className="loader3" id="loader3"></div>
-              </div>
-              <div className="card-footer">
-                <button className="btn btn-success w-100">Source Code</button>
-              </div>
-            </div>
-          </div>
-          <div className="col-md-3">
-            <div className="card">
-              <div className="card-body h-250">
-                <div className="loader4" id="loader4">
-                  Loading...
-                </div>
-              </div>
-              <div className="card-footer">
-                <button className="btn btn-success w-100">Source Code</button>
-              </div>
-            </div>
-          </div>
-          <div className="col-md-3">
-            <div className="card">
-              <div className="card-body h-250">
-                <div className="loader5" id="loader5">
-                  Loading...
-                </div>
-              </div>
-              <div className="card-footer">
-                <button className="btn btn-success w-100">Source Code</button>
-              </div>
-            </div>
-          </div>
-          <div className="col-md-3">
-            <div className="card">
-              <div className="card-body h-250">
-                <div className="loader6" id="loader6">
-                  Loading...
-                </div>
-              </div>
-              <div className="card-footer">
-                <button className="btn btn-success w-100">Source Code</button>
-              </div>
-            </div>
-          </div>
-          <div className="col-md-3">
-            <div className="card">
-              <div className="card-body h-250">
-                <div className="loader7" id="loader7">
-                  Loading...
-                </div>
-              </div>
-              <div className="card-footer">
-                <button className="btn btn-success w-100">Source Code</button>
-              </div>
-            </div>
-          </div>
-          <div className="col-md-3">
-            <div className="card">
-              <div className="card-body h-250">
-                <div className="loader8" id="loader8">
-                  .
-                </div>
-              </div>
-              <div className="card-footer">
-                <button className="btn btn-success w-100">Source Code</button>
-              </div>
-            </div>
+              );
+            })}
           </div>
         </div>
       </div>
-    </div>
-  );
-};
-
-export default Spinner;
+    );
+  }
+}
